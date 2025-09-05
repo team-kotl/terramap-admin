e01c3f540b2f5fdafc62b10beeff73711102429c
Freskkie Encarnacion <fresenc112233@gmail.com>
2025-09-05 11:19:40 +0800

    Fix sidebar, need more adjustmens

--- src/components/core/Sidebar.jsx
+++ src/components/core/Sidebar.jsx
@@ -3,13 +3,17 @@ import { LayoutDashboard, Users, Map, BarChart3 } from "lucide-react"
 
 const Sidebar = () => {
     return (
-        <aside className="w-64 h-screen bg-gray-200 text-secondary p-4 shadow-lg shadow-gray-700/100">
-            <h2 className="text-xl font-bold mb-6">DENR</h2>
-            <ul className="space-y-3">
+        <aside className=" w-64 h-screen bg-gray-200 text-secondary p-4 shadow-lg shadow-gray-700/100">
+            <div className=" mt-5 mb-8 flex flex-row items-center justify-center gap-2">
+                <img src="src\assets\DENR-logo.svg" alt="DENR Logo" className="w-18 h-18"/>
+                <h2 className="text-xl font-bold mb-6 text-black mt-5">DENR - CAR</h2>
+            </div>
+            <hr className="border-t border-gray-400 mb-6" />
+            <ul className="space-y-8 mt-8">
                 <li>
                     <NavLink to="/" 
                         className={({ isActive }) =>
-                            `flex items-center gap-3 hover:text-primary ${
+                            `flex items-center gap-5 hover:text-primary ${
                                 isActive ? "text-primary font-bold" : ""}`}>
                         <LayoutDashboard size={20} /> Overview
                     </NavLink>
@@ -17,15 +21,15 @@ const Sidebar = () => {
                 <li>
                     <NavLink to="/users" 
                         className={({ isActive }) =>
-                            `flex items-center gap-3 hover:text-primary ${
+                            `flex items-center gap-5 hover:text-primary ${
                                 isActive ? "text-primary font-bold" : ""}`}>
-                        <Users size={20} /> Users
+                        <Users size={20}  /> Users
                     </NavLink>
                 </li>
                 <li>
                     <NavLink to="/atlas-activity" 
                         className={({ isActive }) =>
-                            `flex items-center gap-3 hover:text-primary ${
+                            `flex items-center gap-5 hover:text-primary ${
                                 isActive ? "text-primary font-bold" : ""}`}>
                         <Map size={20} /> Atlas Activity
                     </NavLink>
@@ -33,7 +37,7 @@ const Sidebar = () => {
                 <li>
                     <NavLink to="/dashboard-activity" 
                         className={({ isActive }) =>
-                            `flex items-center gap-3 hover:text-primary ${
+                            `flex items-center gap-5 hover:text-primary ${
                                 isActive ? "text-primary font-bold" : ""}`}>
                         <BarChart3 size={20} /> Dashboard Activity
                     </NavLink>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
+
 export default {
     content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                sans: ['Poppins', 'sans-serif'],
+            },
+        },
     },
     plugins: [],
 }
