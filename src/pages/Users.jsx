cc12545cc44114537148032c9423a2d040a5ba9b
cayeelii <carielmagajoyce@gmail.com>
2025-09-14 16:22:42 +0800

    Added modal for edit user

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -2,18 +2,57 @@ import { Search, CirclePlus, Pencil, Trash, CircleX } from "lucide-react";
 import { useState } from "react";
 
 const Users = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isAddModalOpen, setIsAddModalOpen] = useState(false);
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
 
   const users = [
-    { staffId: "ST001", username: "janedoe", role: "Admin", remarks: "Benguet",},
-    { staffId: "ST002", username: "justincabuena", role: "Staff", remarks: "Benguet",},
-    { staffId: "ST003", username: "andrewigs", role: "Staff", remarks: "Ifugao",},
-    { staffId: "ST004", username: "bryce", role: "Manager", remarks: "Kalinga",},
-    { staffId: "ST005", username: "stephdrew", role: "Staff", remarks: "Mt. Province",},
+    {
+      staffId: "ST001",
+      username: "janedoe",
+      role: "Admin",
+      remarks: "Benguet",
+    },
+    {
+      staffId: "ST002",
+      username: "justincabuena",
+      role: "Staff",
+      remarks: "Benguet",
+    },
+    {
+      staffId: "ST003",
+      username: "andrewigs",
+      role: "Staff",
+      remarks: "Ifugao",
+    },
+    {
+      staffId: "ST004",
+      username: "bryce",
+      role: "Manager",
+      remarks: "Kalinga",
+    },
+    {
+      staffId: "ST005",
+      username: "stephdrew",
+      role: "Staff",
+      remarks: "Mt. Province",
+    },
     { staffId: "ST006", username: "morrows", role: "Staff", remarks: "Abra" },
-    { staffId: "ST007", username: "petergrf", role: "Admin", remarks: "Kalinga",},
-    { staffId: "ST008", username: "mgnfox", role: "Staff", remarks: "Apayao" },
-    { staffId: "ST009", username: "lee", role: "Staff", remarks: "Ifugao" },
+    {
+      staffId: "ST007",
+      username: "petergrf",
+      role: "Admin",
+      remarks: "Kalinga",
+    },
+    { staffId: "ST008", 
+      username: "mgnfox", 
+      role: "Staff", 
+      remarks: "Apayao" 
+    },
+    { staffId: "ST009", 
+      username: "lee", 
+      role: "Staff", 
+      remarks: "Ifugao" 
+    },
   ];
 
   return (
@@ -36,7 +75,7 @@ const Users = () => {
         <h1 className="text-3xl font-bold text-green-800">Users</h1>
         <button
           onClick={() => {
-            setIsModalOpen(true);
+            setIsAddModalOpen(true);
           }}
           className="flex items-center gap-2 bg-green-700 hover:bg-green-800 text-white text-sm px-3 py-2 rounded-md"
         >
@@ -67,7 +106,12 @@ const Users = () => {
                 <td className="px-2 py-3">{user.role}</td>
                 <td className="px-4 py-3">{user.remarks}</td>
                 <td className="px-4 py-3">
-                  <button className="text-gray-500 hover:text-blue-500">
+                  <button
+                    onClick={() => {
+                      setIsEditModalOpen(true);
+                    }}
+                    className="text-gray-500 hover:text-blue-500"
+                  >
                     <Pencil className="h-5 w-5" />
                   </button>
                   <button className="text-red-400 hover:text-red-500 ml-3">
@@ -80,14 +124,14 @@ const Users = () => {
         </table>
       </div>
 
-      {isModalOpen && (
-        <div className="fixed inset-0 bg-black/60 flex items-center justify-center z-50 p-4">
+      {isAddModalOpen && (
+        <div className="fixed inset-0 bg-black/60 flex items-center justify-center p-4">
           <div className="bg-white rounded-xl shadow-2xl w-full max-w-md relative transform transition-all duration-300 ease-out">
             <div className="bg-green-800 rounded-t-xl px-6 py-4">
               <div className="flex justify-between items-center">
                 <h2 className="text-xl font-bold text-white">Add New User</h2>
                 <button
-                  onClick={() => setIsModalOpen(false)}
+                  onClick={() => setIsAddModalOpen(false)}
                   className="text-green-100 hover:text-white hover:bg-green-700 rounded-full p-1 transition-colors duration-200"
                 >
                   <CircleX className="h-6 w-6" />
@@ -141,12 +185,101 @@ const Users = () => {
                   />
                 </div>
 
-                <div className="pt-2 flex justify-center">
+                <div className="pt-4 flex space-x-3">
+                  <button
+                    type="button"
+                    onClick={() => setIsAddModalOpen(false)}
+                    className="flex-1 px-6 py-3 bg-gray-200 text-black rounded-lg font-semibold transform hover:scale-[1.02] active:scale-[0.98] transition-all duration-200"
+                  >
+                    Cancel
+                  </button>
                   <button
                     type="submit"
-                    className="px-15 py-3 bg-green-700 hover:from-green-700 hover:to-green-800 text-white rounded-lg font-semibold shadow-lg hover:shadow-xl transform hover:scale-[1.02] active:scale-[0.98] transition-all duration-200"
+                    className="flex-1 px-6 py-3 bg-green-700 text-white rounded-lg font-semibold transform hover:scale-[1.02] active:scale-[0.98] transition-all duration-200"
                   >
-                    Confirm
+                    Update User
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {isEditModalOpen && (
+        <div className="fixed inset-0 bg-black/60 flex items-center justify-center p-4">
+          <div className="bg-white rounded-xl shadow-2xl w-full max-w-md relative transform transition-all duration-300 ease-out">
+            <div className="bg-blue-700 rounded-t-xl px-6 py-4">
+              <div className="flex justify-between items-center">
+                <h2 className="text-xl font-bold text-white">Edit User</h2>
+                <button
+                  onClick={() => setIsEditModalOpen(false)}
+                  className="text-blue-100 hover:text-white hover:bg-blue-500 rounded-full p-1 transition-colors duration-200"
+                >
+                  <CircleX className="h-6 w-6" />
+                </button>
+              </div>
+            </div>
+
+            <div className="px-6 py-6">
+              <form className="space-y-5">
+                <div className="space-y-1">
+                  <label className="block text-sm font-semibold text-gray-700">
+                    Username
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="Enter username"
+                    className="w-full px-4 py-3 border-2 border-gray-200 rounded-lg focus:outline-none focus:border-blue-500  placeholder-gray-400"
+                  />
+                </div>
+
+                <div className="space-y-1">
+                  <label className="block text-sm font-semibold text-gray-700">
+                    Role
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="Enter role"
+                    className="w-full px-4 py-3 border-2 border-gray-200 rounded-lg focus:outline-none focus:border-blue-500  placeholder-gray-400"
+                  />
+                </div>
+
+                <div className="space-y-1">
+                  <label className="block text-sm font-semibold text-gray-700">
+                    Remarks
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="Enter remarks"
+                    className="w-full px-4 py-3 border-2 border-gray-200 rounded-lg focus:outline-none focus:border-blue-500  placeholder-gray-400"
+                  />
+                </div>
+
+                <div className="space-y-1">
+                  <label className="block text-sm font-semibold text-gray-700">
+                    New Password
+                  </label>
+                  <input
+                    type="password"
+                    placeholder="Enter new password (optional)"
+                    className="w-full px-4 py-3 border-2 border-gray-200 rounded-lg focus:outline-none focus:border-blue-500  placeholder-gray-400"
+                  />
+                </div>
+
+                <div className="pt-4 flex space-x-3">
+                  <button
+                    type="button"
+                    onClick={() => setIsEditModalOpen(false)}
+                    className="flex-1 px-6 py-3 bg-gray-200 text-gray-700 rounded-lg font-semibold transform hover:scale-[1.02] active:scale-[0.98] transition-all duration-200"
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    type="submit"
+                    className="flex-1 px-6 py-3 bg-blue-700 text-white rounded-lg font-semibold transform hover:scale-[1.02] active:scale-[0.98] transition-all duration-200"
+                  >
+                    Update User
                   </button>
                 </div>
               </form>
